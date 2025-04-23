9a6ccd4c494ecd6fcba86872f019e3203266f86e
Fran Sanchez <56896216+francosan10@users.noreply.github.com>
2025-04-23 14:39:23 -0400

    Add files via upload

---
+++ SwiftThinkingBasicsBootcamp.playground/Pages/SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+print(greeting)
+print(greeting)
+print(greeting)
+
+// this is a comment inline
+
+/*
+ this is
+ a multiline
+ comment
+ */
+
+// naming conventions
+// let is a constant
+// Camel CAse
+let firstGreeting = "Hello, world"
